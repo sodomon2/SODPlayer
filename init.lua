fa74ab7377bd705799f106d8b6785523b49c9480
Diego Díaz <sodomon2@gmail.com>
2020-07-02 16:00:42 -0400

    actualizando init.lua

--- init.lua
+++ init.lua
@@ -14,6 +14,10 @@ GLib      = lgi.GLib                  -- para el treeview
 Gdk       = lgi.Gdk
 Gtk       = lgi.require('Gtk', '3.0') -- El objeto GTK
 Gst       = lgi.require("Gst", "1.0")
+GdkX11    = lgi.GdkX11
+if tonumber(Gst._version) >= 1.0 then
+   GstVideo = lgi.GstVideo
+end
 
 print(Gst._version)
 assert    = lgi.assert
@@ -22,8 +26,13 @@ builder   = Gtk.Builder()
 assert(builder:add_from_file('vistas/player.ui'))
 ui = builder.objects
 
+function quit()
+    Gtk.main_quit()
+    main_loop:quit()
+end 
+
 function ui.main_window:on_destroy()
-  Gtk.main_quit()
+    quit()
 end
 
 function ui.menu_about_item:on_button_press_event()
