45c5d216df7a2fdbd983091c7c0d164b6c252470
Diego Díaz <sodomon2@gmail.com>
2020-07-02 15:58:45 -0400

    actualizando codigo

--- components/video.lua
+++ components/video.lua
@@ -6,10 +6,14 @@
  @date      19.06.2020 15:58:49 -04
 --]]  
 
-pipeline  = Gst.Pipeline.new('pipeline')
-play      = Gst.ElementFactory.make('playbin', 'play')
-vsink     = Gst.ElementFactory.make('xvimagesink', 'sink')
-main_loop = GLib.MainLoop()
+pipeline  	= Gst.Pipeline.new('pipeline')
+play      	= Gst.ElementFactory.make('playbin', 'play')
+colorspace  = Gst.ElementFactory.make('videoconvert', 'colorspace')
+            or Gst.ElementFactory.make('ffmpegcolorspace', 'colorspace')
+scale       = Gst.ElementFactory.make('videoscale', 'scale')
+rate        = Gst.ElementFactory.make('videorate', 'rate')
+sink        = Gst.ElementFactory.make('xvimagesink', 'sink')
+main_loop 	= GLib.MainLoop()
 
 local function bus_callback(bus, message)
 	if message.type.ERROR then
@@ -18,6 +22,9 @@ local function bus_callback(bus, message)
 	elseif message.type.EOS then
 		print 'end of stream'
 		pipeline.state = 'READY'
+    elseif message.type.STATE_CHANGED then
+      local old, new, pending = message:parse_state_changed()
+      print(string.format('state changed: %s->%s:%s', old, new, pending))
 	end
 
 	return true
@@ -27,6 +34,7 @@ local btn_play_trigger = true
 function ui.btn_play:on_clicked()
 	media_name = ui.load_media:get_filename()
 	play.uri = 'file://' .. media_name 
+  	ui.img_media_state.icon_name = 'media-playback-pause'
 
 	GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1,function()
 		local duration = pipeline:query_duration(Gst.Format.TIME)
@@ -51,6 +59,8 @@ pipeline.bus:add_watch(GLib.PRIORITY_DEFAULT, bus_callback)
 
 function ui.btn_stop:on_clicked()
   pipeline.state = 'NULL'
+  main_loop:quit()
+  ui.media_slider:set_value(0)
 end  
 
 function ui.media_slider:on_value_changed(id)
@@ -65,7 +75,6 @@ function ui.media_slider:on_value_changed(id)
 end
 
 function ui.video:on_realize()
-   print(self:get_xid())
-   vsink:set_window_handle(ui.main_window:get_xid())
+    print(self.window:get_xid())
+    sink:set_window_handle(self.window:get_xid())
 end
-
