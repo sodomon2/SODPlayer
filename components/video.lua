0f488fc5bd15a32cac331ac5fd2b573d65922a71
Diego Díaz <sodomon2@gmail.com>
2020-07-07 17:48:30 -0400

    funcion para el envio de la salida de video hacia la drawingarea

--- components/video.lua
+++ components/video.lua
@@ -4,16 +4,11 @@
  @version   0.2
  @author    Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com>
  @date      19.06.2020 15:58:49 -04
---]]  
+--]]
 
-pipeline  	= Gst.Pipeline.new('pipeline')
-play      	= Gst.ElementFactory.make('playbin', 'play')
-colorspace  = Gst.ElementFactory.make('videoconvert', 'colorspace')
-            or Gst.ElementFactory.make('ffmpegcolorspace', 'colorspace')
-scale       = Gst.ElementFactory.make('videoscale', 'scale')
-rate        = Gst.ElementFactory.make('videorate', 'rate')
-sink        = Gst.ElementFactory.make('xvimagesink', 'sink')
-main_loop 	= GLib.MainLoop()
+pipeline    = Gst.Pipeline.new('pipeline')
+play        = Gst.ElementFactory.make('playbin', 'play')
+main_loop   = GLib.MainLoop()
 
 local function bus_callback(bus, message)
 	if message.type.ERROR then
@@ -33,8 +28,8 @@ end
 local btn_play_trigger = true
 function ui.btn_play:on_clicked()
 	media_name = ui.load_media:get_filename()
-	play.uri = 'file://' .. media_name 
-  	ui.img_media_state.icon_name = 'media-playback-pause'
+	play.uri = 'file://' .. media_name
+  ui.img_media_state.icon_name = 'media-playback-pause'
 
 	GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1,function()
 		local duration = pipeline:query_duration(Gst.Format.TIME)
@@ -54,6 +49,7 @@ function ui.btn_play:on_clicked()
 	pipeline.state = 'READY'
 end
 
+
 pipeline:add_many(play)
 pipeline.bus:add_watch(GLib.PRIORITY_DEFAULT, bus_callback)
 
@@ -62,7 +58,7 @@ function ui.btn_stop:on_clicked()
   main_loop:quit()
   ui.media_slider:set_value(0)
   ui.img_media_state.icon_name = 'media-playback-start'
-end  
+end
 
 function ui.media_slider:on_value_changed(id)
 	if btn_play_trigger then
@@ -75,7 +71,8 @@ function ui.media_slider:on_value_changed(id)
 	end
 end
 
+-- Retarget video output to the drawingarea.
 function ui.video:on_realize()
-    print(self.window:get_xid())
-    sink:set_window_handle(self.window:get_xid())
+   print(self.window:get_xid())
+   play:set_window_handle(self.window:get_xid())
 end
