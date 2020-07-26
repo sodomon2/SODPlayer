6f6c0064c862ecc0e267515cd565e2c959082a81
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2020-07-26 13:14:10 -0400

    añadiendo preferencias para el statusicon

--- components/preferences.lua
+++ components/preferences.lua
@@ -15,3 +15,15 @@ function ui.btn_preferences_save:on_clicked()
     ui.preferences_window:hide()
     print(font_size)
 end
+
+menu_preferences_tray      = Gtk.CheckMenuItem()
+menu_preferences_tray_item = menu_preferences_tray.new_with_mnemonic('_Show icon in system tray')
+ui.menu_preferences:add({menu_preferences_tray_item})
+
+function menu_preferences_tray_item:on_button_press_event()
+    if (self:get_active()) then
+      ui.tray:set_visible(false)
+    else
+      ui.tray:set_visible(true)
+    end
+end
