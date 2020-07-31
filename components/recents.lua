10b7e7385b92f52c30aedfdb1378b18d824a4f66
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2020-07-31 04:24:13 -0400

    añadiendo nuevas funciones

--- components/recents.lua
+++ components/recents.lua
@@ -6,17 +6,24 @@
  @date      31.07.2020 02:24:50 -04
 --]]
 
+menu_archive_recent = Gtk.ImageMenuItem {
+    label    = "Recientes",
+    image = Gtk.Image {stock = "gtk-directory"}
+}
+
 recents_item = conf.history.recents
 
 function create_submenu(depth)
 	if depth < 1 then return nil end
 	local menu_recent = Gtk.Menu()
 	for i, uri in pairs(recents_item) do
+        ui.menu_archive:add({menu_archive_recent})
 		local item = Gtk.MenuItem {
 			label = ("%d. %s"):format(i, uri),
 			submenu = create_submenu(depth - 1, true),
 			on_button_press_event = function ()
 				-- @TODO: esto se puede mejorar
+                stop_media()
 				play.uri = 'file://' .. uri
 			    play_media()
 			end
@@ -26,4 +33,10 @@ function create_submenu(depth)
 	return menu_recent
 end
 
-ui.menu_archive_item_recent.submenu = create_submenu(1)
+if #recents_item < 1 then
+    ui.menu_archive:popdown({menu_archive_recent})
+    return
+end 
+
+menu_archive_recent.submenu = create_submenu(1)
+
