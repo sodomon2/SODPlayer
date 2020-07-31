716539324cec5fa0c7672b1032a34a4bf2687982
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2020-07-31 16:46:56 -0400

    actualizacion menor

--- components/subtitles.lua
+++ components/subtitles.lua
@@ -5,8 +5,10 @@
  @author    Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com>
  @date      10.07.2020 03:01:54 -04
 --]]  
+
+subtitle_size = conf.subtitles.font_size
+
 function subtitle_load()
-    subtitle_size = conf.subtitles.font_size
     srt_file = ui.file_subtitle_chooser:get_filename(chooser)
     play.suburi = 'file://' .. srt_file
     play.subtitle_font_desc = subtitle_size
@@ -21,3 +23,5 @@ end
 function ui.btn_subtitle_close:on_clicked()
     ui.file_subtitle_chooser:hide()
 end
+
+ui.subtitle_font_widget:set_font(subtitle_size)
