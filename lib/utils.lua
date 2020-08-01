6c89bb1631a704378ce42e249e8880e81679c6d0
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2020-08-01 19:36:51 -0400

    añadiendo nueva libreria

---
+++ lib/utils.lua
@@ -0,0 +1,28 @@
+--[[--
+ @package   SODplayer
+ @filename  utils.lua  
+ @version   1.5
+ @author    Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com> 
+ @date      01.08.2020 19:22:11 -04
+--]]
+
+local utils = class('utils')
+
+function utils:split(str,sep)
+    local sep, fields = sep or ":", {}
+    local pattern = string.format("([^%s]+)", sep)
+    str:gsub(pattern, function(c) fields[#fields+1] = c end)
+    return fields
+end
+
+function utils:path_name(uri)
+    local uri   = uri or ''
+    local _turi =  utils:split(uri,'/')
+    local _name = _turi[#_turi]
+    local _path = '/' .. table.concat(_turi, '/')
+    table.remove(_turi,(#_turi))
+    result =  { name = _name, path = _path }
+    return result
+end
+
+return utils
