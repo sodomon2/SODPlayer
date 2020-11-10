987bb48ba3f1d0d5647012895d8b447d5076d13f
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2020-11-10 15:50:02 -0400

    guardado de volumen implementado

--- components/SODPlayer-volume.lua
+++ components/SODPlayer-volume.lua
@@ -6,6 +6,16 @@
  @date      19.06.2020 15:43:09 -04
 --]]  
 
+
+function ui.volume_control:on_value_changed()
+	if  (ui.volume_control:get_value() == 100) then
+		conf.general.volume = 100
+    else
+		conf.general.volume = (math.floor(ui.volume_control:get_value())/100)
+    end
+	config:save(('%s/sodplayer.json'):format(dir), conf)
+end
+
 function ui.btn_volume:on_clicked()
 	ui.menu_volume:show()
 end
@@ -14,5 +24,7 @@ function ui.volume_control:on_value_changed()
 	ui.volume_control:set_range(0, 100 )
 	local value = (math.floor(ui.volume_control:get_value())/100)
 	print( value )
-	play.volume = value
+	play.volume = conf.general.volume
 end
+
+ui.volume_control:set_value(conf.general.volume)
